db65a867ca42e94b98401481e056c367ea815f4a
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2025-02-02 01:00:43 -0500

    Fix extraneous non-props attributes (data-v-inspector)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
 
     devtools: {
         enabled: true,
+        componentInspector: false,
     },
 
     css: ['normalize.css/normalize.css', '@/assets/css/app.css'],
