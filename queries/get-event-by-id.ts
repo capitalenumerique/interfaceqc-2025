8beda82e39b808ef95aafab137c30760b8253eb8
Krystina Hamel <krystina.hamel@libeo.com>
2025-05-14 08:10:46 -0400

    [HOTFIX] Ajuster le maximum pour la programmation

--- queries/get-event-by-id.ts
+++ queries/get-event-by-id.ts
@@ -2,7 +2,7 @@ import { gql } from 'graphql-request';
 
 export const GET_EVENT_BY_ID = gql`
     query EventById($eventId: ID!) {
-        eventPerson(eventId: $eventId, cursor: { first: 200 }) {
+        eventPerson(eventId: $eventId, cursor: { first: 500 }) {
             nodes {
                 id
                 firstName
